8aad2827c50849a6cfa164b3fafde467235c7979
yinwenfeng_company <2209041586@qq.com>
2022-07-12 19:31:45 +0800

    添加syslog文件

--- fopen/demon_fopen.c
+++ fopen/demon_fopen.c
@@ -2,50 +2,69 @@
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define FILEPATH ("./test.txt")
+#define FILEPATH ("./message.log")
 
+/*****************************************************************************
+*  函 数 名：main
+*  功能描述：按时时间提取syslog
+*  输入参数：int argc            
+*            char const *argv[]  
+*  输出参数：无
+*  返 回 值：int
+*  
+*  修改历史：
+*         1. 日    期：2022年7月12日
+*            作    者：yinwenfeng
+*            修改内容：新生成函数
+*  
+*****************************************************************************/
 int main(int argc, char const *argv[])
 {
-	char *buff = NULL;
-	unsigned long buffSize = 0;
-	char line[512] = {0};
+	char *sBuff = NULL;
+	unsigned int ulBuffSize = 0;
+	char szLine[512] = {0};
+	time_t pCurrentTime;
+	char *sCurrentTime = NULL;
 	
-	
-    FILE *file = fopen(FILEPATH, "r");
-	if (!file)
+    FILE *pFile = fopen(FILEPATH, "r");
+	if (!pFile)
 	{
 		printf("can not open %s: %s\n", FILEPATH, strerror(errno));
 		exit(EXIT_FAILURE);
 	}
 
-	/* 一个字符读取 */
-//	char ch = fgetc(file);
-//	while( EOF != ch)
-//	{
-//		buffSize++;
-//		buff = (char *)realloc(buff, buffSize);
-//		buff[buffSize-1] = ch;
-//		ch = fgetc(file);
-//	}
-//	buff[buffSize] = '\0';
-//	printf("%s", buff);
-//	free(buff);
+	/* 获取当前日期 */
+	ctime(&pCurrentTime);
+	printf("\r\n \033[1;35m[--ywf--]func: %s, line: %d\033[0m: sCurrentTime = %s\n", __FUNCTION__, __LINE__, sCurrentTime);
 
+	
 	/* 一次读取一行 */
-	fseek(file, 0, SEEK_SET);
-	while (NULL != fgets(line, sizeof(line), file))
+	fseek(pFile, 0, SEEK_SET);
+	sBuff = (char *)malloc(1);
+	while (NULL != fgets(szLine, sizeof(szLine), pFile))
 	{
-		buffSize += strlen(line);
-		buff = (char *)realloc(buff, buffSize);
+		//提取时间
 		
-		strcat(buff, line);
-	}
-	printf("%s", buff);
-	free(buff);
-	fclose(file);
 
-	printf("\r\n \033[1;35m[--ywf--]func: %s, line: %d\033[0m: file size %fkb\n", __FUNCTION__, __LINE__, buffSize/1024.0);
+		
+		ulBuffSize += strlen(szLine);
+		sBuff = (char *)realloc(sBuff, ulBuffSize);
+		if (NULL == sBuff )
+		{
+		    perror("realloc fail");
+			return -1;
+		}
+		
+		
+		strcat(sBuff, szLine);
+	}
+	printf("%s", sBuff);
+	free(sBuff);
+	fclose(pFile);
+
+	printf("\r\n \033[1;35m[--ywf--]func: %s, szLine: %d\033[0m: file size %fkb\n", __FUNCTION__, __LINE__, ulBuffSize/1024.0);
     return 0;
 }
 
